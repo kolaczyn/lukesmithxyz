0ea024718ff3cde9e858f7a9b1e873ea2a821deb
Paweł Kołaczyński <kolaczyn@protonmail.com>
2020-11-01 22:54:28 +0100

    Improved buttons Also added button styles to RSS links on index.html

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // it copies RSS feed link if you click a link
-
 const rssLinks = [...document.getElementById('rss-links').children]
 rssLinks.forEach(link => {
     link.addEventListener('click', ()=> {

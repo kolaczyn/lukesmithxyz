49527c5b3f17dbe36ac87d20bcf82948e477c0c4
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-01-06 15:50:38 +0100

    Add toast, fix responsivity issues

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -1,6 +1,33 @@
+const createToast = (message) => {
+    removeOldToasts()
+
+    const toast = document.createElement('div')
+    toast.classList.add('toast')
+    toast.classList.add('content')
+
+    const paragraph = document.createElement('p')
+    paragraph.innerHTML = message
+
+    toast.appendChild(paragraph)
+
+    const body = document.querySelector('body')
+    body.appendChild(toast)
+
+    setTimeout(() => toast.remove(), 2250)
+    toast.addEventListener('click', () => toast.remove())
+}
+
+const removeOldToasts = () => {
+    const oldToasts = document.querySelectorAll('.toast')
+    oldToasts.forEach(toast => {
+        toast.remove();
+    })
+}
+
 const copyElements = document.querySelectorAll("[toCopy]");
 copyElements.forEach(element => {
     element.addEventListener('click', () => {
         window.navigator.clipboard.writeText(element.getAttribute('toCopy'));
+        createToast('Copied to clipboard.')
     });
 });
